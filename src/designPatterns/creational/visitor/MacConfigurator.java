883b85541f29a0f9759ad43f83e76d212ba24c00
mmahawar <mmahawar@gmail.com>
2014-01-25 17:12:53 -0500

    visitor pattern

---
+++ src/designPatterns/creational/visitor/MacConfigurator.java
@@ -0,0 +1,20 @@
+package designPatterns.creational.visitor;
+
+public class MacConfigurator implements RouterVisitor{
+
+	@Override
+	public void visit(DLinkRouter router) {
+		System.out.println("DLinkRouter Configuration for Mac complete !!");
+	}
+
+	@Override
+	public void visit(TPLinkRouter router) {
+		System.out.println("TPLinkRouter Configuration for Mac complete !!");
+	}
+
+	@Override
+	public void visit(LinkSysRouter router) {
+		System.out.println("LinkSysRouter Configuration for Mac complete !!");
+	}
+
+}
---
+++ src/designPatterns/creational/visitor/TestVisitorPattern.java
@@ -0,0 +1,47 @@
+package designPatterns.creational.visitor;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestVisitorPattern {
+
+	private MacConfigurator macConfigurator;
+    private LinuxConfigurator linuxConfigurator;
+    private DLinkRouter dlink;
+    private TPLinkRouter tplink;
+    private LinkSysRouter linksys;
+     
+    @Before
+    public void setUp()
+    {
+        macConfigurator = new MacConfigurator();
+        linuxConfigurator = new LinuxConfigurator();
+         
+        dlink = new DLinkRouter();
+        tplink = new TPLinkRouter();
+        linksys = new LinkSysRouter();
+    }
+     
+    @Test
+    public void testDlink()
+    {
+        dlink.accept(macConfigurator);
+        dlink.accept(linuxConfigurator);
+    }
+     
+    @Test
+    public void testTPLink()
+    {
+        tplink.accept(macConfigurator);
+        tplink.accept(linuxConfigurator);
+    }
+     
+    @Test
+    public void testLinkSys()
+    {
+        linksys.accept(macConfigurator);
+        linksys.accept(linuxConfigurator);
+    }
+
+}
